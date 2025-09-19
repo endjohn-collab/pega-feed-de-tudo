d8de9c2d63c892659e5e4785f826c6fc060fa95d
endjohn-collab <endjohn@gmail.com>
2025-09-19 12:42:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const axios = require("axios");
+const http = require("http");
 
-axios.get("https://jsonplaceholder.typicode.com/todos/1")
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.end("Hello, Node.js!");
+});
+
+server.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000");
+});
