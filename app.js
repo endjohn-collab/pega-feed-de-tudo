588829561719d32c224e28dd33e2a0571e5d714b
endjohn-collab <endjohn@gmail.com>
2025-09-19 12:48:50 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-const http = require("http");
+res.end("Hello, Node.js!");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("Hello, Node.js!");
-});
 
-server.listen(3000, () => {
-  console.log("Servidor rodando em http://localhost:3000");
-});
